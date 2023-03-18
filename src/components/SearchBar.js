3d280cbaa68a130ab4211a820849042b9d86f5e6
RossCr0ss <sobechko.r@gmail.com>
2023-03-18 13:57:02 +0200

    Add input handling to state.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+  state = { term: "" };
+
   render() {
     return (
       <div className="ui segment">
         <form className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" />
+            <input value={this.state.term} type="text" onChange={(event) => this.setState({ term: event.target.value })} />
           </div>
         </form>
       </div>
